0c0df3d02b4dd74bd3ffb89ce861d9d0c5a34865
akshanshverma28 <107884868+akshanshverma28@users.noreply.github.com>
2025-07-18 16:12:49 +0530

    Update __init__.py

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -1,16 +1,4 @@
-#!/usr/bin/python
-#-----------------------------------------------------------------------------
-# Name:        __init__.py
-#
-# Purpose:     The regular package init module to init the customer "lib".
-#
-# Author:      Yuancheng Liu
-#
-# Created:     2014/01/15
-# Version:     v_0.1.0
-# Copyright:   Copyright (c) 2014 LiuYuancheng
-# License:     MIT License    
-#-----------------------------------------------------------------------------
+
 
 """
 Regular Package Info
